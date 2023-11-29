1ac9a28e92c6b8300d70c6d3eeed8c1464cb33ff
Alireza <alireza@Alirezas-MBP.lan>
2023-11-29 12:36:16 +1100

    Alter the creat order endpoint to retrive the user id from the JWT inside the request header

--- Routes/orders.py
+++ Routes/orders.py
@@ -1,20 +1,24 @@
-from fastapi import APIRouter, HTTPException
-from pydantic import BaseModel
+from typing import Annotated
+from fastapi import APIRouter, HTTPException, Header
+from jose import JWTError, jwt
 from DB.fakeDB import shopping_carts_db
 from Models.ShoppingCart import ShoppingCart
 
 router = APIRouter()
 
-class Id(BaseModel):
-    id: str
-
 # POST /v1/orders : Create an empty shopping cart for the user
 @router.post("/v1/orders", response_model=ShoppingCart)
-async def create_shopping_cart(user_id: Id):
-    if user_id.id in shopping_carts_db:
+async def create_shopping_cart(auth_token: Annotated[str, Header()]):
+    try:
+        payload = jwt.decode(auth_token, "secret", algorithms="HS256")
+        user_id: str = payload.get("user_id")
+        if user_id is None:
+            raise HTTPException(status_code=400, detail="The provided token does not have user id, please try logging in again.")
+    except JWTError:
+        raise HTTPException(status_code=401, detail="Could not validate credentials")
+    if user_id in shopping_carts_db:
         raise HTTPException(status_code=400, detail="Shopping cart already exists for this user")
-    
-    shopping_cart = ShoppingCart(cart_id=user_id.id)
-    shopping_carts_db[user_id.id] = shopping_cart
+    shopping_cart = ShoppingCart(cart_id=user_id)
+    shopping_carts_db[user_id] = shopping_cart
     return shopping_cart
    
