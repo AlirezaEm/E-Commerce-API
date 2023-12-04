1915217fd3fd7539a721ec9cc1421ad2712dadad
Alireza <alireza@Alirezas-MBP.modem>
2023-12-04 11:46:20 +1100

    Use .resource instead of .client, when creating an instance of the DB. Also update the requiremenst.txt

--- Models/ShoppingCart.py
+++ Models/ShoppingCart.py
@@ -6,27 +6,3 @@ class ShoppingCart(BaseModel):
     cart_id: str
     items: List[Item] = []
     state: str = "open"
-
-    @classmethod
-    def from_dynamodb_item(cls, dynamodb_item: dict):
-        item_data = dynamodb_item.get('Item', {})
-        cart_id = item_data.get('cart_id', {}).get('S', '')
-        
-        # Extracting items if available
-        items_raw = item_data.get('items', {}).get('L', [])
-        items_list = []
-        for item in items_raw:
-            item_data = item['M']
-            items_list.append(Item(
-                item_id=item_data['item_id']['S'],
-                name=item_data['name']['S'],
-                description=item_data.get('description', {}).get('S'),
-                price=float(item_data['price']['N']),
-                quantity=int(item_data['quantity']['N'])
-            ))
-        items = items_list
-
-        # Extracting state if available
-        state = item_data.get('state', {}).get('S', 'open')
-
-        return cls(cart_id=cart_id, items=items, state=state)
--- Routes/orders.py
+++ Routes/orders.py
@@ -3,7 +3,6 @@ from fastapi import APIRouter, HTTPException, Header
 from jose import JWTError, jwt
 from Models.ShoppingCart import ShoppingCart
 from DB.fakeDB import get_ddb_instance
-from Models.DynamoModel import DynamoModel
 
 router = APIRouter()
 
@@ -19,26 +18,23 @@ async def create_shopping_cart(auth_token: Annotated[str, Header()]):
     except JWTError:
         raise HTTPException(status_code=401, detail="Could not validate credentials")
     
-    existing_item = ddb.get_item(TableName="e-commerce", Key={"cart_id": {"S": user_id}})
+    existing_item = ddb.get_item(Key={"cart_id": user_id})
     if existing_item.get("Item"):
         raise HTTPException(status_code=400, detail="Shopping cart already exists for the user")
 
     shopping_cart = ShoppingCart(cart_id=user_id)
-    item = DynamoModel().model_to_dynamodb(shopping_cart)
-    ddb.put_item(TableName="e-commerce", Item=item)   
+    ddb.put_item(Item=shopping_cart.dict())   
     
-
     return shopping_cart
    
 # DELETE /v1/orders/uuid
 @router.delete("/v1/orders/{uuid}", response_model=ShoppingCart)
 def delete_shopping_cart(uuid: str):
-    existing_item = ddb.get_item(TableName="e-commerce", Key={"cart_id": {"S": uuid}})
+    existing_item = ddb.get_item(Key={"cart_id": uuid})
     
     if not existing_item.get("Item"):
         raise HTTPException(status_code=404, detail="Shopping cart was not found for this user")
     
-    ddb.delete_item(TableName="e-commerce", Key={"cart_id": {"S": uuid}})
-    item = ShoppingCart.from_dynamodb_item(existing_item)
-    
-    return item
+    ddb.delete_item(Key={"cart_id": uuid})
+    deleted_item = ShoppingCart(**existing_item.get('Item'))
+    return deleted_item
