ef1626a092f2c4d5b46d4a1178c7ea0f5982a00d
Alireza <alireza@Alirezas-MBP.modem>
2023-12-06 23:04:13 +1100

    Check the ownership of the order

--- Routes/orders.py
+++ Routes/orders.py
@@ -65,17 +65,6 @@ def checkout_shopping_cart(uuid: str, auth_token: Annotated[str, Header()]):
     except JWTError:
         raise HTTPException(status_code=401, detail="Could not validate credentials")
         
-# PATCH /v1/orders/uuid : Add/remove an item to/from the cart
-@router.patch("/v1/orders/{uuid}")
-def update_shopping_cart(uuid: str, items: list[Item], auth_token: Annotated[str, Header()]):
-    try:
-        payload = jwt.decode(auth_token, "secret", algorithms="HS256")
-        user_id: str = payload.get("user_id")
-        if user_id is None:
-            raise HTTPException(status_code=401, detail="The provided token does not have user id, please try logging in again.")
-    except JWTError:
-        raise HTTPException(status_code=401, detail="Could not validate credentials")
-    
     try:
         existing_item = ddb.get_item(Key={"cart_id": uuid})
     except Exception as e:
@@ -98,6 +87,43 @@ def update_shopping_cart(uuid: str, items: list[Item], auth_token: Annotated[str
                                         ExpressionAttributeNames={'#state': 'state'},
                                         ExpressionAttributeValues={':new_state': 'PAID'},
                                         ReturnValues='ALL_NEW'
+        )
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    
+    return updated_item.get("Attributes")
+        
+# PATCH /v1/orders/uuid : Add/remove an item to/from the cart
+@router.patch("/v1/orders/{uuid}")
+def update_shopping_cart(uuid: str, items: list[Item], auth_token: Annotated[str, Header()]):
+    try:
+        payload = jwt.decode(auth_token, "secret", algorithms="HS256")
+        user_id: str = payload.get("user_id")
+        if user_id is None:
+            raise HTTPException(status_code=401, detail="The provided token does not have user id, please try logging in again.")
+    except JWTError:
+        raise HTTPException(status_code=401, detail="Could not validate credentials")
+    
+    try:
+        existing_item = ddb.get_item(Key={"cart_id": uuid})
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+        
+    if not existing_item.get("Item"):
+        raise HTTPException(status_code=404, detail="Shopping cart was not found for this user")
+    
+    if existing_item.get("Item").get("owner_id") != user_id:
+        raise HTTPException(status_code=403, detail="You are not authorized to checkout this shopping cart")
+
+    serialized_items = [item.dict() for item in items]
+
+    try:
+        updated_item = ddb.update_item(
+            Key={"cart_id": uuid},
+            UpdateExpression="SET #items = :new_items",
+            ExpressionAttributeNames={'#items': 'items'},
+            ExpressionAttributeValues={":new_items": serialized_items},
+            ReturnValues="ALL_NEW"
         )
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
